db68ac36fee88110d6523699c7501757d2f33c6e
embeddedlinuxworkshop <139722851+embeddedlinuxworkshop@users.noreply.github.com>
2023-08-29 03:37:18 +0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <math.h>
+
+
+void helloworld();
+
+
+/**
+ * 1. Link with the secret Library.
+ * Dyn. and statically.
+*/
+int main ()
+{
+    //secret();
+
+    helloworld();
+}
